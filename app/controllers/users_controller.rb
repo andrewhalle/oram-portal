eab69b9614dcee0d20b4c3247a35d2d9ece96cbf
Alice Ma <alicema@berkeley.edu>
2017-07-31 04:42:27 +0000

    fixed the codeclimate error by deleting one line in the show method of the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
 	def show
 		@curr_admin = current_admin
 		@user = User.find(params[:id])
-		@form_hash = {}
 		@caseworker_names = []
 		@notes = @user.notes.reverse
 		@updocs = Updoc.where(user_id: @user.id).all
@@ -13,13 +12,9 @@ class UsersController < ApplicationController
 				caseworker_id = ownership.admin_id
 				@caseworker_names.append(Admin.find_by_id(caseworker_id).full_name)
 			end
-		# else
-		# 	@caseworker_names.append('This user has no caseworkers.')
 		end
 		if !@user.events.last.nil?
 			@last_event_message = @user.events.last.message
-		# else
-		# 	@last_event_message = 'This user has had no events before!'
 		end
 		if @user.role == "referrer"
 			if !@user.forms.empty? && !@user.forms.where(form_type: 1).empty?
